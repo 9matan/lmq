4bbd6107b9ff51f2d54f44f208c1017acbc2b003
Yamatan <oleg.yavorovich@gmail.com>
2023-11-15 17:54:06 +0200

    Add a variadic macro for Logging

--- lmq/engine/include/lmq/system/Logging/lmqLogging.h
+++ lmq/engine/include/lmq/system/Logging/lmqLogging.h
@@ -1,29 +1,47 @@
 #pragma once
 
+enum class ELogLevel
+{
+    Error = 0,
+    Warning,
+    Info,
+    Debug
+};
+
+#define lmq_LOGGING_BUFFER_SIZE 256
+
 #ifdef ARDUINO
 
-#define lmq_LOG_DEBUG(msg) lmqLogDebug(msg);
-#define lmq_LOG_INFO(msg) lmqLogInfo(msg);
-#define lmq_LOG_WARNING(msg) lmqLogWarning(msg);
-#define lmq_LOG_ERROR(msg) lmqLogError(msg);
+#define lmq_PLATFORM_SUPPORT_LOGGING 1
 
 class __FlashStringHelper;
 
-void lmqLogDebug(const __FlashStringHelper* ifsh);
-void lmqLogInfo(const __FlashStringHelper* ifsh);
-void lmqLogWarning(const __FlashStringHelper* ifsh);
-void lmqLogError(const __FlashStringHelper* ifsh);
+void lmqLog(const ELogLevel logLevel, const __FlashStringHelper* ifsh);
+void lmqLog(const ELogLevel logLevel, const char* str);
+void lmqLogf(const ELogLevel logLevel, const char* format, ...);
 
-void lmqLogDebug(const char* str);
-void lmqLogInfo(const char* str);
-void lmqLogWarning(const char* str);
-void lmqLogError(const char* str);
+#endif // ARDUINO
 
-#else // ARDUINO
+#ifdef lmq_PLATFORM_SUPPORT_LOGGING
 
-#define lmq_LOG_DEBUG(msg)
-#define lmq_LOG_INFO(msg);
-#define lmq_LOG_WARNING(msg)
-#define lmq_LOG_ERROR(msg)
+#define lmq_LOG(logLvl, msg) lmqLog(logLvl, msg)
+#define lmq_LOGF(logLvl, fmt, ...) lmqLogf(logLvl, fmt, __VA_ARGS__)
 
-#endif
+#else // lmq_PLATFORM_SUPPORT_LOGGING
+
+#warning ("Warning: the platform does not support logging")
+
+#define lmq_LOG(logLvl, msg)
+#define lmq_LOGF(logLvl, fmt, ...)
+
+#endif // !lmq_PLATFORM_SUPPORT_LOGGING
+
+#define lmq_LOG_DEBUG(msg) lmq_LOG(ELogLevel::Debug, msg)
+#define lmq_LOG_INFO(msg) lmq_LOG(ELogLevel::Info, msg)
+#define lmq_LOG_WARNING(msg) lmq_LOG(ELogLevel::Warning, msg)
+#define lmq_LOG_ERROR(msg) lmq_LOG(ELogLevel::Error, msg)
+
+#define lmq_LOGF_DEBUG(fmt, ...) lmq_LOGF(ELogLevel::Debug, fmt, __VA_ARGS__)
+#define lmq_LOGF_INFO(fmt, ...) lmq_LOGF(ELogLevel::Info, fmt, __VA_ARGS__)
+#define lmq_LOGF_WARNING(fmt, ...) lmq_LOGF(ELogLevel::Warning, fmt, __VA_ARGS__)
+#define lmq_LOGF_ERROR(fmt, ...) lmq_LOGF(ELogLevel::Error, fmt, __VA_ARGS__)
